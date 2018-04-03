3380a857d605bef065e171327130214c034914bd
Christopher <christopher.loubassou@hotmail.fr>
2018-04-04 00:05:25 +0200

    Creation of first component and first rendering

---
+++ README.md
@@ -0,0 +1,4 @@
+# Practice of React and Redux
+
+The project is about a creation of shoplist with React. This project will help the understanding of React
+After we gonna use Redux to understand the usin of global state 
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <h3>Liste de courses</h3>
+                <div>En construction</div>
+            </div>
+        );
+    }
+}
+
+export default App;
